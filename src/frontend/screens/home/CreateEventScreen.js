28c97274f1556cbd4acd50319122907c5aab591b
Jimmy Wei <icewing1996@gmail.com>
2020-08-31 06:16:09 -0400

    toast

--- src/frontend/screens/home/CreateEventScreen.js
+++ src/frontend/screens/home/CreateEventScreen.js
@@ -27,6 +27,35 @@ import * as ImagePicker from 'expo-image-picker';
 import * as firebase from 'firebase';
 import { withFirebaseHOC } from "~/../firebase/config/Firebase";
 const uuidv4 = require('random-uuid-v4');
+import Toast from 'react-native-root-toast';
+
+
+// Add a Toast on screen.
+let toast = Toast.show('This is a message', {
+    duration: Toast.durations.LONG,
+    position: Toast.positions.BOTTOM,
+    shadow: true,
+    animation: true,
+    hideOnPress: true,
+    delay: 0,
+    onShow: () => {
+        // calls on toast\`s appear animation start
+    },
+    onShown: () => {
+        // calls on toast\`s appear animation end.
+    },
+    onHide: () => {
+        // calls on toast\`s hide animation start.
+    },
+    onHidden: () => {
+        // calls on toast\`s hide animation end.
+    }
+});
+
+// You can manually hide the Toast, or it will automatically disappear after a `duration` ms timeout.
+setTimeout(function () {
+    Toast.hide(toast);
+}, 500);
 
 
 console.disableYellowBox = true;
