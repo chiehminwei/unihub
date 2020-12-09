9e100bc0e1755480b538633aff9a4df3116640f7
Jimmy Wei <icewing1996@gmail.com>
2020-12-09 00:34:39 -0500

    auth ok

--- src/frontend/navigation/HomeStackScreen.js
+++ src/frontend/navigation/HomeStackScreen.js
@@ -7,18 +7,19 @@ import HomeScreen  from '~/screens/home/HomeScreen';
 import FilterScreen from '~/screens/home/FilterScreen';
 import PlannerScreen from '~/screens/home/PlannerScreen';
 import CreateEventScreen from '~/screens/home/CreateEventScreen';
-
+import EventDetailScreen from '~/screens/home/EventDetailScreen';
 
 
 const HomeStack = createStackNavigator();
 
 export function HomeStackScreen() {
   return (
-    <HomeStack.Navigator initialRouteName="Home">
+    <HomeStack.Navigator initialRouteName="Home" >
       <HomeStack.Screen options={{headerShown: false}} name="Home" component={HomeScreen} />
       <HomeStack.Screen name="Filter" component={FilterScreen}  />
       <HomeStack.Screen name="Planner" component={PlannerScreen} />
       <HomeStack.Screen name="Create" component={CreateEventScreen} />
+      <HomeStack.Screen options={{headerShown: false}} name="EventDetail" component={EventDetailScreen} />      
     </HomeStack.Navigator>
   );
 }
